55aa72d623fcb82adc2b63bbb6f5191e96b332c6
harshana47 <pabodhaharshana15.com>
2025-01-26 19:23:53 +0530

    1/26 19:23

--- src/main/java/org/example/assignmentee/adminControllers/CategoryDeleteServlet.java
+++ src/main/java/org/example/assignmentee/adminControllers/CategoryDeleteServlet.java
@@ -1,11 +1,13 @@
 package org.example.assignmentee.adminControllers;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,9 +16,8 @@ import java.sql.SQLException;
 
 @WebServlet(name = "CategoryDeleteServlet", value = "/deleteCategory")
 public class CategoryDeleteServlet extends HttpServlet {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/ecommerce";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "harshima@147"; // Replace with your database password
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource dataSource;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -24,7 +25,7 @@ public class CategoryDeleteServlet extends HttpServlet {
         String id = request.getParameter("id"); // Get the category ID from the request
 
         if (id != null) {
-            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            try (Connection connection = dataSource.getConnection()) {
                 String query = "DELETE FROM categories WHERE id = ?";
                 try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
                     preparedStatement.setInt(1, Integer.parseInt(id));
